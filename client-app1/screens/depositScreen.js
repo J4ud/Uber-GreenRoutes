77d2163ed23b494e51e360923454843030f2e309
lulitohhh <luisa.mariaprf@gmail.com>
2024-11-25 23:24:41 -0500

    Css cliente 1 casi completo

--- client-app1/screens/depositScreen.js
+++ client-app1/screens/depositScreen.js
@@ -3,12 +3,15 @@ import { router, socket } from "../routes.js";
 export default function renderScreen5() {
   const app = document.getElementById("app");
   app.innerHTML = `
-        <div class="container">
-            <div class="content">
-             <p class="title">Deposita tu producto</p>
-             <p class="description">Deposita tu plástico dentro de nuestro recipiente para ser parte del cambio.</p>
-        <div class="illustration">
-          <img src="images/recycle-bin.svg" alt="Recycle bin">
+        <header class="navbar">
+        <img src="images/frame1.webp" alt="Logo Uber" class="navbar-logo">
+    </header>
+    <div class="page-container">
+      <div class="body-content">
+        <p class="headline">Deposita tu producto</p>
+        <p class="paragraph-info">Deposita tu plástico dentro de nuestro recipiente para ser parte del cambio.</p>
+        <div class="image-container">
+          <img src="images/papelera.webp" alt="Recycle bin">
         </div>
         <button id="simular2">Simular</button>
       </div>
